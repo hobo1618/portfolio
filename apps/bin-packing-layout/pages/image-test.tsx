b2a13fa371eaa10800eb7b353ab8ef4af1ad5988
Hobo Spaceship <hobdenw@gmail.com>
2022-09-10 22:39:37 +0200

    test algorithm

--- apps/bin-packing-layout/pages/image-test.tsx
+++ apps/bin-packing-layout/pages/image-test.tsx
@@ -1,15 +1,50 @@
 import type { NextPage } from "next";
 import Image from "next/image";
+import Packer from "packer";
 import objects from "data/objects.json";
 
+let blocks = [
+  { width: 300, height: 300, playdo: "sadwich" },
+  { width: 100, height: 100 },
+  { width: 100, height: 300 },
+  { width: 80, height: 80 },
+  { width: 80, height: 80 },
+  { width: 80, height: 80 },
+  { width: 80, height: 80 },
+  { width: 80, height: 80 },
+  { width: 80, height: 80 },
+  { width: 80, height: 80 },
+  { width: 80, height: 80 },
+  { width: 80, height: 80 },
+];
+
 const ImagesTest: NextPage = () => {
+  const packer = new Packer(500, 500);
+  packer.fit(blocks);
+
   return (
-    <div>
-      <Image
+    <div style={{ width: "500px", background: "blue", height: "500px" }}>
+      {blocks.map((block) => {
+        if (block.fit) {
+          return (
+            <div
+              style={{
+                position: "absolute",
+                left: `${block.fit.x}px`,
+                width: `${block.width}px`,
+                top: `${block.fit.y}px`,
+                height: `${block.height}px`,
+                border: "1px dotted gray",
+              }}
+            ></div>
+          );
+        }
+      })}
+      {/* <Image
         width={objects[0].width}
         height={objects[0].height}
         src={`/assets/${objects[0].href}`}
-      />
+      /> */}
     </div>
   );
 };
