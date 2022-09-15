9d31ea071e9d1a7462b56a0b187fadb48394bf82
Hobo Spaceship <hobdenw@gmail.com>
2022-09-15 22:10:45 +0200

    create css module for blur image

--- apps/bin-packing-layout/components/BlurImage.tsx
+++ apps/bin-packing-layout/components/BlurImage.tsx
@@ -1,46 +1,15 @@
 import Image from "next/image";
+import styles from "components/BlurImage.module.css";
 
 const BlurImage = ({ width, height, href }) => {
-  const css = `
-  .imageEffects {
-    animation: fadeInAnimation ease-in 1s;
-    animation-iteration-count: 1;
-    animation-fill-mode: forwards;
-  }
-  @keyframes fadeInAnimation {
-    0% {
-      opacity: 0;
-      filter: blur(20px);
-      transform: scale(1.1)
-    }
-    10% {
-      opacity: 0;
-      filter: blur(4px);
-      transform: scale(1.1)
-    }
-    40% {
-      opacity: .1;
-      filter: blur(2px);
-      transform: scale(1.05)
-    }
-    100% {
-      opacity: 1;
-      filter: blur(0px)
-      transform: scale(1)
-    }
-  }
-  `;
   return (
-    <>
-      <style>{css}</style>
-      <Image
-        width={width}
-        height={height}
-        src={`/assets/${href}`}
-        className="imageEffects"
-      />
-    </>
+    <Image
+      width={width}
+      height={height}
+      src={`/assets/${href}`}
+      className={styles.imageEffects}
+    />
   );
 };
 
-export default BlurImage
+export default BlurImage;
