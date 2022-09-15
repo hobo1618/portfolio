7d5fffe8d8d1f6054d3c034e7b5e13d40b23ff17
Hobo Spaceship <hobdenw@gmail.com>
2022-09-15 11:23:01 +0200

    create FFD Infinite height algorithm

--- apps/bin-packing-layout/algorithms/FirstFitInfiniteHeight.ts
+++ apps/bin-packing-layout/algorithms/FirstFitInfiniteHeight.ts
@@ -1,48 +1,108 @@
-class FirstFitInfiniteHeight {
+class FFDInfiniteHeight {
   width: number;
-  nextRowY: number;
-  container: object; // defines the origin of the new rectangle, along with its width
-  // and height
+  root: object;
+  height: number;
 
-  constructor(width: number, height: number) {
+  constructor(width: number) {
     this.width = width;
-    // this.height = height;
-    this.nextRowY = 0;
-    this.container = { x: 0, y: 0, width: this.width, nextRowY: this.nextRowY };
+    this.height = 0;
+    this.root = { x: 0, y: 0, width, height: this.height };
   }
 
-  fit(blocks) {
+  fit(blocks: []) {
+    let node;
     blocks.sort((a, b) => b.height - a.height || b.width - a.width);
-    blocks.map((block) => {
-      block.width <= this.container.width
-        ? this.placeInCurrentRow(block, this.container)
-        : this.placeInNewRow(block, this.container);
+    this.root.height = blocks[0].height;
+    blocks.map((block: object) => {
+      // if we find a good root, then we set node = root
+      // i.e. this is where we'll place our block
+
+      // in the second iteration, this means that
+      // node = origin-root.right effectively
+
+      // when both node.down and node.right return null,
+      // then node = null and we
+      // continue with the next block, reinitializing the
+      // search from this.root
+      node = this.findNode(this.root, block.width, block.height);
+      // assuming node != null, we process the block placement
+      // by calling splitNode and assigning a "fit" property
+      // to the block, which tells us where it should be placed in
+      // the bin
+      if (node) block.fit = this.splitNode(node, block.width, block.height);
     });
-
-    return blocks;
   }
 
-  placeInCurrentRow(block, container) {
-    if (container.x == 0 && container.y == 0) {
-      this.nextRowY = block.height;
+  findNode(root: object, blockWidth: number, blockHeight: number) {
+    if (root.used)
+      return (
+        // if root.right is undefined or root.down is undefined,
+
+        // First we call findNode on root.right.
+        // Let's say root.right.used is undefined
+        // then we skip to the else if statement
+        // where, say, we determine the block is too
+        // wide to fit in root.right.
+        this.findNode(root.right, blockWidth, blockHeight) ||
+        // this.findNode(root.right)
+        // therefore returns null and we pass to
+        // this line of code where we test the same idea.
+        this.findNode(root.down, blockWidth, blockHeight)
+      );
+    // if the root is unused and the block fits,
+    // set node = root
+    // on the first iteration, this means we place
+    // the first block at the origin
+    // on the second iteration, this means we place the second block
+    // to the right, say, and then set
+    // node = root, which is equivalent to node = root.right
+    else if (blockWidth <= root.width && blockHeight <= root.height)
+      return root;
+    // if the block is unused but doesn't fit in the root, set node = null
+    else if (blockWidth <= root.width && root.x == 0) {
+      root.height = blockHeight
+      return root;
     }
-    block.x = container.x;
-    block.y = container.y;
-    container.x = container.x + block.width;
-    container.width = container.width - block.width;
-    return block;
+    else return null
   }
 
-  placeInNewRow(block, container) {
-    container.y = this.nextRowY;
-    this.nextRowY = this.nextRowY + block.height;
-    container.width = this.width;
-    block.x = 0;
-    block.y = container.y;
-    container.x = block.width;
-    container.width = this.width - block.width;
-    return block;
+  splitNode(node: object, width: number, height: number) {
+    /* 
+        this function:
+        
+        1. adds properties to the root: used, down, right
+        By adding a property to the root, we are creating the
+        tree structure that we will search recursively. On the
+        second iteration, the function adds properties to root.right,
+        creating root.right.right and root.right.down objects.
+        etc.
+
+        2. returns "node", which has this structure:
+          {
+            x: number,
+            y: number,
+            width: number,
+            height: number
+          }
+
+        This object is assigned to block.fit
+          block.fit = node
+    */
+    node.used = true;
+    node.down = {
+      x: node.x,
+      y: node.y + height,
+      width: node.width,
+      height: node.height - height,
+    };
+    node.right = {
+      x: node.x + width,
+      y: node.y,
+      width: node.width - width,
+      height: height,
+    };
+    return node;
   }
 }
 
-export default FirstFitInfiniteHeight;
+export default FFDInfiniteHeight;
