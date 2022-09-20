997ba19fe0de7247fe807cc78f4d9f308cbaeeb7
Hobo Spaceship <hobdenw@gmail.com>
2022-09-20 22:19:52 +0200

    fix filter to allow iterable object

--- apps/image-gallery/utils/filters.ts
+++ apps/image-gallery/utils/filters.ts
@@ -1,20 +1,28 @@
 export const includesEvery = (filteredArr, filterArr) => {
-  const filteredSet = new Set(filteredArr)
-  return filterArr.every((item) => filteredSet.has(item))
+  const filteredSet = new Set(filteredArr);
+  return filterArr.every((item) => filteredSet.has(item));
 };
 
 export const includesSome = (filteredArr, filterArr) => {
-  const filteredSet = new Set(filteredArr)
-  return filterArr.some((item) => filteredSet.has(item))
+  const filteredSet = new Set(filteredArr);
+  return filterArr.some((item) => filteredSet.has(item));
 };
 
-export const keepItemByOrWithinAndBetween = (filteredArr, filterObj) => {
+export const keepItemByOrWithinAndBetween = (filteredArr, filter) => {
   let keep = true;
-  Object.keys(filterObj).map((key) => {
-    if (filterObj[key].length == 0) return;
-    if (!includesSome(filteredArr, filterObj[key])) return (keep = false);
-  });
-  return keep;
+  if (Array.isArray(filter)) {
+    filter.map((filterRow) => {
+      if (filterRow.length == 0) return;
+      if (!includesSome(filteredArr, filterRow)) return (keep = false);
+    });
+  } else {
+    Object.keys(filter).map((key) => {
+      let filterRow = filter[key];
+      if (filterRow.length == 0) return;
+      if (!includesSome(filteredArr, filterRow)) return (keep = false);
+    });
+    return keep;
+  }
 };
 
 export const advancedFilter = (array, property, filterArr, filterFunction) => {
@@ -23,20 +31,3 @@ export const advancedFilter = (array, property, filterArr, filterFunction) => {
   );
   return filteredArr;
 };
-
-
-
-// export const includesEvery = (filteredArr, filterArr) => {
-//   return filterArr.every((item) => filteredArr.includes(item))
-// };
-
-// export const includesSome = (filteredArr, filterArr) => {
-//   return filterArr.some((item) => filteredArr.includes(item))
-// };
-
-// export const advancedFilter = (array, property, filterArr, filterFunction) => {
-//   const filteredArr = array.filter((item) =>
-//     filterFunction(item[property], filterArr)
-//   );
-//   return filteredArr;
-// };
